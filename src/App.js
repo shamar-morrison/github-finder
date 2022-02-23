41401928a95329a62d08b61ad9ee01aa11a3c1de
shamar-morrison <shamar.webdev@gmail.com>
2022-02-23 07:40:35 -0500

    added more console.logs

--- src/App.js
+++ src/App.js
@@ -71,6 +71,23 @@ const App = () => {
   };
 
   console.log('test');
+  const log = () => {
+    console.log('test1');
+    console.log('test2');
+    console.log('test3');
+    console.log('test4');
+    console.log('test5');
+    console.log('test6');
+    console.log('test7');
+    console.log('test8');
+    console.log('test9');
+    console.log('test0');
+    console.log('test');
+    console.log('test');
+    console.log('test');
+    console.log('test');
+    console.log('test');
+  };
 
   // Get user repos
   const getUserRepos = async username => {
